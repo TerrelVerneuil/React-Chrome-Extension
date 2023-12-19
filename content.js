0687ba383959b78d10ce26c4a8f3d156604f7acc
T404 <44128398+TerrelVerneuil@users.noreply.github.com>
2023-12-19 09:05:13 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,19 +1,22 @@
-console.log("Content script loaded");
-document.getElementById("Start").addEventListener("click", function() {
-    console.log("Start!");
+
+
+
+document.getElementById("Stop").addEventListener("click", function() {
+    chrome.runtime.sendMessage({ action: "toggleTracking", isTracking: false });
 });
+
+
+document.getElementById("Start").addEventListener("click", function() {
+    chrome.runtime.sendMessage({ action: "toggleTracking", isTracking: true });
+});
+
 document.getElementById("Stop").addEventListener("click", function() {
     console.log("Stop!");
 });
 document.getElementById("Account").addEventListener("click", function() {
-    console.log("Account!");
-});
-
-document.getElementById("Account").addEventListener("click", function() {
     // Open a new tab with the specified URL when the button is clicked
 
     const newPageURL = chrome.runtime.getURL("Account.html");
-
     chrome.tabs.create({ url: newPageURL });
 });
 
